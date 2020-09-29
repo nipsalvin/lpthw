f1bfa3fde4872a195226bcf020fed0ed10e6a8a6
nipsalvin <68748209+nipsalvin@users.noreply.github.com>
2020-09-29 09:29:25 +0300

    Add files via upload

---
+++ ex29.py
@@ -0,0 +1,33 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 6
+
+if people >= dogs:
+    print ("People are equal or greater than dogs.")
+
+if people <= dogs:
+    print("People are less or equal to dogs.")
+
+
+if people == dogs:
+    print("People ae dogs.")
+
+if people != dogs:
+    print("more dogs or people ??")
+
