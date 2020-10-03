8674906661ba0c188881621d89458c00506866eb
nipsalvin <68748209+nipsalvin@users.noreply.github.com>
2020-10-03 09:06:05 +0300

    Add files via upload
    ex35.py is a modified version of the same

---
+++ ex33.py
@@ -0,0 +1,15 @@
+i = 0
+numbers = []
+
+for i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+    
+    i = i + 1
+    print("Numbers now: ",numbers)
+    print(f"At the bottom i is {i}")
+
+print("The number: ")
+
+for num in numbers:
+    print(num)
---
+++ ex34.py
@@ -0,0 +1,2 @@
+animals = ['bear', 'python 3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
